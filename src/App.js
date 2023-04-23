39a794f887bb25a85a05cb1412b81e5c8603e034
Moussa <manuscrit974@gmail.com>
2023-04-23 18:04:07 +0200

    en cours

--- src/App.js
+++ src/App.js
@@ -17,11 +17,11 @@ const App = () => {
         setData(data.data);
       })
       .catch((err) => console.log(err));
-  }, []);
+  });
 
   return (
     <div className="container mx-auto mt-12">
-      <h1 className="text-center text-9xl font-bold my-8">All Flags 🏴</h1>
+      <h1 className="text-center text-6xl font-bold my-8">All Flags 🏴</h1>
       <OptionCountries
         setSortPopulation={setSortPopulation}
         setRange={setRange}
@@ -39,15 +39,15 @@ const App = () => {
       )}
       <div className="grid grid-cols-8 gap-4 ">
         {data
-
           .slice(0, range)
-
           .sort((a, b) =>
             sortPopulation
               ? b.population - a.population
               : a.population - b.population
           )
-          .filter((continent) => continent.continents[0] === sortContinent)
+          // .filter((continent) =>
+          //   continent ? continent.continents[0] === sortContinent : continent
+          // )
           .map((flag, index) => (
             <Card key={index} flag={flag} />
           ))}
